1b6832c1d899d1fa15ffdbb461c3b4621baafadc
GhadirHussen <ghadirhussen92@gmail.com>
2021-03-11 19:54:19 +0200

    First commit

---
+++ MapForeachRduceFilterSet.js
@@ -0,0 +1,61 @@
+// function Test(){
+//     const items = new Set(["apple","orange"]);
+//     items.add("banana");
+//     // items.add("apple");
+//     // items.add("orange");
+//     items.add("banana");
+
+    
+//     items.forEach(item => console.log(item));
+// }
+    
+
+// function Test(){
+//     const myMap = new Map([["Hussen",1234],["Neel",5678]]);
+//     // const myMap= new Map();
+//     // myMap.set("Hussen" ,1234);
+//     // myMap.set ("Neel" ,5678);
+        
+    
+//     myMap.forEach(key => console.log(key));
+//     myMap.forEach((key,value )=> console.log(value));
+//     myMap.forEach((key,value) => console.log(key +": " + value));
+// }
+
+// function Test(){
+
+// ////console array with random up to 100    
+//     const arr = Array.from({length:100},()=> Math.floor(Math.random()*100));
+//     // console.log( arr.map(()=> Math.floor(Math.random()*100)));
+//     console.log(arr)
+
+
+//     console.log(`${"-".repeat(20)}`);
+
+// ////console the number is > 90    
+//     arr.map(()=> {
+//     let max = Math.floor(Math.random()*100);
+//     if(max > 90){
+//         console.log(max);
+//     }
+//     });
+
+//     console.log(`${"-".repeat(20)}`);
+
+// //// console all number is ended with 7
+//     arr.map(()=> {
+//     let randomNum = Math.floor(Math.random()*100);
+//     let sum = 7 ;
+//     for(let i=0 ; i < arr.length ; i++){
+//         if(randomNum === sum){
+//             console.log(randomNum);
+//         }
+//         sum += 10;
+//     }
+// });
+
+
+// }
+
+
+   
